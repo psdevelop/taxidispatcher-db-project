b3e7b0fc79f54b766cff25f4e27f6b14a9086bed
Stanislav Poltarokov <psdevelop@yandex.ru>
2020-01-20 20:15:14 +0300

    инициализация настроек после обновления

--- TD_CHANGES_DB/единый пакет обновления БД/SQLQueryTDUpdateDDL.sql
+++ TD_CHANGES_DB/единый пакет обновления БД/SQLQueryTDUpdateDDL.sql
@@ -1951,3 +1951,11 @@ END
 
 GO
 
+UPDATE Objekt_vyborki_otchyotnosti
+SET addit_rem_params = '"csw":"1","MSREF":"no","sopc":"yes","rstnok":"1","ovsdssum":"0","RESTTXM":"yes","CSLDN":"yes","TICAB":"yes","NETL":"no","UBLK":"no","CHKTAR":"yes","regbdst":"6","regbctm":"0","dstd":"0","rsbt":"1","tmminsp":"3","tmmaxsp":"220","tmminds":"2","tmmaxds":"800","UTDBL":"yes","UGPT":"yes","geocpref":"","SKINST":"yes",',
+start_free_time = 0, start_free_distance = 0;
+
+GO
+
+
+
